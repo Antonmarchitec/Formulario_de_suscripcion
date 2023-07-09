9a78debf9ab11a22537245dc2c0254ee7a3e3aa7
mi nombre <desarrollowebaps@gmail.com>
2023-07-09 00:30:25 -0400

    03_formulario__poppup

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let campoInput = document.querySelector("#campoInput").value
+let form__buttom = document.querySelector("#form__buttom")
+
+
+form__buttom.addEventListener("click", () =>{
+    console.log("presionasdte el boton")
+} )
+
+
+
