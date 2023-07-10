978cc6906c6cf4d823bcacf4bcd4b1807128e5e6
mi nombre <desarrollowebaps@gmail.com>
2023-07-09 23:42:52 -0400

    04_formulario__poppup

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-let campoInput = document.querySelector("#campoInput").value
 let form__buttom = document.querySelector("#form__buttom")
+let cont__poppup = document.querySelector("#cont__poppup")
 
+
+
+
+
+
+
+
+const validarCorreo = () =>{
+    let correo = document.querySelector("#campoInput").value
+    let patron = /^[\w\.-]+@[\w\.-]+\.\w+$/;
+
+    if (patron.test(correo)) {
+        console.log("puedes entrar")
+    } else {
+        console.log("no puesdes entrar")
+    }
+}
 
 form__buttom.addEventListener("click", () =>{
-    console.log("presionasdte el boton")
-} )
-
-
-
+    validarCorreo()
+})
