c4b3eee2a0b94d281f7f410c3a3af4c37653cc10
mi nombre <desarrollowebaps@gmail.com>
2023-07-10 00:30:17 -0400

    05_formulario__poppup

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 let form__buttom = document.querySelector("#form__buttom")
-let cont__poppup = document.querySelector("#cont__poppup")
-
-
-
-
-
+//let cont__poppup = document.querySelector("#cont__poppup")
+//let cont__poppup__exit = document.querySelector("#cont__poppup__exit")
+let poppup__btn = document.querySelector("#poppup__btn")
 
 
 
@@ -14,11 +11,25 @@ const validarCorreo = () =>{
 
     if (patron.test(correo)) {
         console.log("puedes entrar")
+        cont__poppup.classList.add("cont__poppup__js")
     } else {
-        console.log("no puesdes entrar")
+        console.log("no puedes entrar")
+        cont__poppup__exit.classList.add("cont__poppup__exit__js")
     }
 }
 
 form__buttom.addEventListener("click", () =>{
-    validarCorreo()
+    validarCorreo() 
+})
+
+
+
+document.querySelector("#poppup__btn").addEventListener("click", ()=>{
+    console.log("salir")
+    cont__poppup.classList.remove("cont__poppup__js")
+})
+
+document.querySelector("#poppup__btn__exit").addEventListener("click", ()=>{
+    console.log("salir")
+    cont__poppup__exit.classList.remove("cont__poppup__exit__js")
 })
