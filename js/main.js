fd76628ca877fc4e4b4672fc49dd71af46b9e341
mi nombre <desarrollowebaps@gmail.com>
2023-07-10 22:27:03 -0400

    06_formulario__poppup

--- js/main.js
+++ js/main.js
@@ -1,35 +1,28 @@
-let form__buttom = document.querySelector("#form__buttom")
-//let cont__poppup = document.querySelector("#cont__poppup")
-//let cont__poppup__exit = document.querySelector("#cont__poppup__exit")
-let poppup__btn = document.querySelector("#poppup__btn")
 
-
-
+//funcion
 const validarCorreo = () =>{
     let correo = document.querySelector("#campoInput").value
     let patron = /^[\w\.-]+@[\w\.-]+\.\w+$/;
 
     if (patron.test(correo)) {
-        console.log("puedes entrar")
+        //console.log("puedes entrar")
         cont__poppup.classList.add("cont__poppup__js")
     } else {
-        console.log("no puedes entrar")
+        //console.log("no puedes entrar")
         cont__poppup__exit.classList.add("cont__poppup__exit__js")
     }
 }
 
-form__buttom.addEventListener("click", () =>{
-    validarCorreo() 
-})
 
-
-
-document.querySelector("#poppup__btn").addEventListener("click", ()=>{
-    console.log("salir")
-    cont__poppup.classList.remove("cont__poppup__js")
-})
-
-document.querySelector("#poppup__btn__exit").addEventListener("click", ()=>{
-    console.log("salir")
-    cont__poppup__exit.classList.remove("cont__poppup__exit__js")
+//Delegando eventos
+document.addEventListener("click", (e)=>{
+    if(e.target.id === "form__buttom"){
+        validarCorreo()
+    }else if(e.target.id === "poppup__btn"){
+        //console.log("salir")
+        cont__poppup.classList.remove("cont__poppup__js")
+    }else if(e.target.id === "poppup__btn__exit"){
+        //console.log("salir")
+        cont__poppup__exit.classList.remove("cont__poppup__exit__js")
+    }
 })
